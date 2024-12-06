1bd279543def6b0ff6e40780374c1fd2d9fc586d
AryaPutra-182 <mukhammadaryap@gmail.com>
2024-12-07 00:53:23 +0700

    laravel update

--- register.php
+++ register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Smart Family Registration</title>
-    <link rel="stylesheet" href="css/register.css" />
+    <link rel="stylesheet" href="css/login.css" />
   </head>
   <body>
     <div class="background-image">
